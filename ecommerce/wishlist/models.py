0bdf9646507b0c478da21737b37b7c74b6a5fad1
JGobinda <jgobinda@insightworkshop.io>
2023-01-06 13:02:14 +0545

    [add] added wishlist app, models

---
+++ ecommerce/wishlist/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+from ecommerce.accounts.models import User
+from ecommerce.product.models import Product
+
+
+class WishList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_wishlists')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='user_products')
+
+    def __str__(self):
+        return f'{self.product.name} + "-->" + {self.user.name}'
